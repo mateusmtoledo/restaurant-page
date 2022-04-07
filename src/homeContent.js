4afba3c2d8ec6976895ee1b6dc43d1992823ca4f
Mateus Mauricio Toledo <mateusmtoledo1@gmail.com>
2022-04-07 17:35:25 -0300

    Add home content

---
+++ src/homeContent.js
@@ -0,0 +1,56 @@
+const displayHome = function() {
+    const content = document.getElementById('content');
+    content.appendChild(hours());
+    content.appendChild(about());
+    content.appendChild(location());
+}
+
+function hours() {
+    const container = document.createElement('div');
+    const h2 = document.createElement('h2');
+    h2.textContent = 'Hours';
+
+    const sunday = document.createElement('p');
+    const monday = document.createElement('p');
+    const tuesday = document.createElement('p');
+    const wednesday = document.createElement('p');
+    const thursday = document.createElement('p');
+    const friday = document.createElement('p');
+    const saturday = document.createElement('p');
+
+    sunday.textContent = 'Sunday: 8am - 8pm';
+    monday.textContent = 'Monday: 6am - 6pm';
+    tuesday.textContent = 'Tuesday: 6am - 6pm';
+    wednesday.textContent = 'Wednesday: 6am - 6pm';
+    thursday.textContent = 'Thursday: 6am - 10pm';
+    friday.textContent = 'Friday: 6am - 10pm';
+    saturday.textContent = 'Saturday: 8am - 10pm';
+
+    container.append(h2, sunday, monday, tuesday, wednesday, thursday, friday, saturday);
+
+    return container;
+}
+
+function about() {
+    const container = document.createElement('div');
+    const h2 = document.createElement('h2');
+    h2.textContent = 'About Us';
+    const p = document.createElement('p');
+    p.textContent = 'Maecenas vitae urna nec erat tempus pellentesque et sit amet ipsum. Sed tempus justo et risus porta condimentum. Vivamus finibus, risus eget vehicula posuere, ligula elit facilisis turpis, ut pellentesque massa elit et nisi. Pellentesque accumsan odio id nisi hendrerit imperdiet. Aliquam erat volutpat. Nunc quis lectus vel nunc consequat elementum. Phasellus volutpat mollis accumsan. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc sem lacus, iaculis id metus vel, auctor consectetur nunc. Duis lacinia id libero scelerisque vulputate. Sed consequat tortor nec lobortis volutpat. Nunc consequat vulputate euismod. Praesent quis vehicula enim. Vivamus accumsan congue sapien. Quisque venenatis varius nunc, porta cursus mi varius sit amet.';
+    
+    container.append(h2, p);
+
+    return container;
+}
+
+function location() {
+    const container = document.createElement('div');
+    const h2 = document.createElement('h2');
+    h2.textContent = 'Location';
+    
+    container.append(h2);
+
+    return container;
+}
+
+export default displayHome;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import pageLoad from './pageLoad.js';
 import './style.css';
+import displayHome from './homeContent.js';
 
 pageLoad();
+displayHome();
