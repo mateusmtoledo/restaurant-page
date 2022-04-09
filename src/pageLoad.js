3fbc2bbfb8e6925ee0707ffae4ffaab9ce73875a
Mateus Mauricio Toledo <mateusmtoledo1@gmail.com>
2022-04-08 22:26:20 -0300

    Add current tab indication

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -22,9 +22,13 @@ function createHeader() {
         // Home 
         const homeNav = document.createElement('li');
         homeNav.textContent = 'Home';
+        homeNav.setAttribute('data-active', '1');
         homeNav.addEventListener('click', () => {
             const content = document.getElementById('content');
             if (content.firstChild.id === 'home-page') return;
+            contactNav.setAttribute('data-active', '0');
+            menuNav.setAttribute('data-active', '0');
+            homeNav.setAttribute('data-active', '1');
             content.removeChild(content.firstChild);
             displayHome();
         });
@@ -32,9 +36,13 @@ function createHeader() {
         // Menu
         const menuNav = document.createElement('li');
         menuNav.textContent = 'Menu';
+        menuNav.setAttribute('data-active', '0');
         menuNav.addEventListener('click', () => {
             const content = document.getElementById('content');
             if (content.firstChild.id === 'menu-page') return;
+            homeNav.setAttribute('data-active', '0');
+            contactNav.setAttribute('data-active', '0');
+            menuNav.setAttribute('data-active', '1');
             content.removeChild(content.firstChild);
             displayMenu();
         });
@@ -42,9 +50,13 @@ function createHeader() {
         // Contact
         const contactNav = document.createElement('li');
         contactNav.textContent = 'Contact';
+        contactNav.setAttribute('data-active', '0');
         contactNav.addEventListener('click', () => {
             const content = document.getElementById('content');
             if (content.firstChild.id === 'contact-page') return;
+            menuNav.setAttribute('data-active', '0');
+            homeNav.setAttribute('data-active', '0');
+            contactNav.setAttribute('data-active', '1');
             content.removeChild(content.firstChild);
             displayContact();
         });
